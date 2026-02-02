43323884e0ed73b73c1396ef9f9f02f7df49fea5
Varun Chimurkar <84261757+varunchimurkar@users.noreply.github.com>
2026-02-02 08:23:10 +0000

    Variable ✅

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 23568
+let accountMail = "varunchimurkar@gmail.com"
+var accountpsd = "12345"
+accountCity = "Nagpur"
+let accountState;
+
+//accountId = 2 
+
+/* let - not Re-declare, Function Scope
+   var - Re-declare, Block Scope
+*/
+
+console.table([ accountId, accountMail, accountpsd, accountCity, accountState])
