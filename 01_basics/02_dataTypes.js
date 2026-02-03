713329a5ace5115c33d105f01aa26ede2ca1258d
Varun Chimurkar <84261757+varunchimurkar@users.noreply.github.com>
2026-02-03 07:19:33 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,16 @@
+let name = "varun"
+let age = 18
+let isLoggedIn = false
+
+
+//number
+//bigint
+//String = ""
+//boolean = true or false
+//null = standalone value (typeof - object)
+//undefined (typeof - undefined)
+//Symbol = unique
+
+//object 
+
+console.log(typeof age)
